e616f60e15efaf97af04b40212003f4704beea82
Sudhindar31 <sudhindarkm@yahoo.com>
2020-12-08 19:36:31 +0530

    update 08-12-2020

--- src/main/java/com/xworkz/page/login/LoginPage.java
+++ src/main/java/com/xworkz/page/login/LoginPage.java
@@ -1,5 +1,24 @@
 package com.xworkz.page.login;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
-	
+	private WebDriver driver;
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	private static final String REGISTERBUTTON_XPATH_KEY = "";
+	private static final String LOGINBUTTON_XPATH_KEY = "";
+
+	public By identifyElementById(String idLocator) {
+		return By.id(idLocator);
+	}
+
+	public By identifyElementByXpath(String xpathLocator) {
+		return By.xpath(xpathLocator);
+	}
+
 }
